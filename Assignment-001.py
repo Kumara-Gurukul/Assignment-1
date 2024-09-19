70133faf5e60652aa2d6c3ecc7f583e7235a2438
kduraira <kumaraprabaharan.durairaj@nokia.com>
2024-09-19 15:05:26 +0530

    hi

---
+++ Assignment-001.py
@@ -0,0 +1,23 @@
+
+
+#1.Define a variable as Integer(1) ,Float(1.0) and String(‘1’) and print and return the value and type of variable.
+
+
+int_var = 1
+float_var = 1.0
+string_var = '1'
+
+print("Integer variable:")
+print("Value:", int_var)
+print("Type:", type(int_var))
+print()
+
+print("Float variable:")
+print("Value:", float_var)
+print("Type:", type(float_var))
+print()
+
+print("String variable:")
+print("Value:", string_var)
+print("Type:", type(string_var))
+print()
